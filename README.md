c6067b842f0691084e4ff0cfa101c42b1f45c862
Mori Keigo <yuzumori.keigo@gmail.com>
2020-07-10 21:57:46 +0900

    jQueryでスプラッシュ画面表示

---
+++ README.md
@@ -0,0 +1,7 @@
+# jQueryでスプラッシュ画面表示
+
+## 参考先
+https://gimmicklog.com/jquery/278/
+
+## jQuery利用方法
+https://handywebdesign.net/2017/09/jquery-for-beginner/
---
+++ sample.js
@@ -0,0 +1,30 @@
+$(function () {
+
+  var h = $(window).height();
+
+
+  $('#wrap').css('display', 'none');
+  $('#loader-bg ,#loader').height(h).css('display', 'block');
+});
+
+$(window).load(function () { //全ての読み込みが完了したら実行
+
+  $('#loader-bg').delay(1000).fadeOut(800);
+  $('#loader').delay(1000).fadeOut(300);  //delayで調整
+  $('#wrap').css('display', 'block');
+});
+
+//ここらから下は消していい。
+
+//10秒たったら強制的にロード画面を非表示
+$(function () {
+
+  setTimeout('stopload()', 10000);
+});
+
+function stopload() {
+
+  $('#wrap').css('display', 'block');
+  $('#loader-bg').delay(900).fadeOut(800);
+  $('#loader').delay(600).fadeOut(300);
+};
